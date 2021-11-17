d2f2224a584d3b81562c54c4639636e514455485
AhmedHajiyev <hajiyev.ahmed01@gmail.com>
2021-11-17 11:39:32 +0400

    Initial Commit

---
+++ KennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KennyGame
+//
+//  Created by Ahmed Hajıyev on 17.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
